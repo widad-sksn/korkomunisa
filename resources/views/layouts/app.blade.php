051663bd4e9be0d59ca4890046dd7d776251e74a
widad-sksn <indofals12345@gmail.com>
2026-07-02 01:50:17 +0700

    style: add favicon to guest and app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ $attributes->has('title') ? $attributes->get('title') . ' - KORKOM UNISA' : 'KORKOM UNISA' }}</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
